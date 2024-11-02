bd366aec9bc3ae28cd8ceee06caebe832991ef47
Guido Flohr <guido.flohr@cantanea.com>
2024-11-02 10:21:45 +0200

    add invoice controller

--- src/invoice/invoice.module.ts
+++ src/invoice/invoice.module.ts
@@ -1,10 +1,11 @@
 /* istanbul ignore file */
 import { Module } from '@nestjs/common';
 
+import { InvoiceController } from './invoice.controller';
 import { InvoiceService } from './invoice.service';
 
 @Module({
 	providers: [InvoiceService],
-	controllers: [],
+	controllers: [InvoiceController],
 })
 export class InvoiceModule {}
