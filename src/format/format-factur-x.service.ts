db5075f3bca4fe1f7888370880f441e7eb671661
Guido Flohr <guido.flohr@cantanea.com>
2024-11-28 09:12:25 +0200

    add missing import

--- src/format/format-factur-x.service.ts
+++ src/format/format-factur-x.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 import { FormatCIIService, FULL_CII, FXProfile } from './format-cii.service';
 import { EInvoiceFormat } from './format.e-invoice-format.interface';
+import { Invoice } from '../invoice/invoice.interface';
 
 @Injectable()
 export class FormatFacturXService
@@ -14,5 +15,11 @@ export class FormatFacturXService
 
 	get fxProfile(): FXProfile {
 		return FULL_CII;
+	}
+
+	generate(invoice: Invoice): string | Buffer {
+		const xml = super.generate(invoice);
+
+		return xml;
 	}
 }
