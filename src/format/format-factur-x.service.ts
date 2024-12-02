6be3aa8a6fc64b45574b3a43d315c428eff9da9f
Guido Flohr <guido.flohr@cantanea.com>
2024-12-02 11:37:53 +0200

    make pdf creation globally available

--- src/format/format-factur-x.service.ts
+++ src/format/format-factur-x.service.ts
@@ -1,7 +1,4 @@
-import { Injectable, Logger } from '@nestjs/common';
-import { spawn } from 'child_process';
-import { promises as fs } from 'fs';
-import * as path from 'path';
+import { Injectable } from '@nestjs/common';
 import {
 	AFRelationship,
 	PDFArray,
@@ -12,8 +9,6 @@ import {
 	PDFNumber,
 	PDFString,
 } from 'pdf-lib';
-import * as tmp from 'tmp-promise';
-import * as url from 'url';
 import { create } from 'xmlbuilder2';
 import { XMLBuilder } from 'xmlbuilder2/lib/interfaces';
 
@@ -105,8 +100,6 @@ export class FormatFacturXService
 	extends FormatCIIService
 	implements EInvoiceFormat
 {
-	private readonly logger = new Logger(FormatFacturXService.name);
-
 	get mimeType(): string {
 		return 'application/pdf';
 	}
@@ -119,21 +112,7 @@ export class FormatFacturXService
 		invoice: Invoice,
 		options: InvoiceServiceOptions,
 	): Promise<string | Buffer> {
-		let pdf: Buffer;
-
-		if (options.pdf) {
-			pdf = options.pdf;
-		} else if (options.data) {
-			pdf = await this.getPdfFromSpreadsheet(
-				options.data.buffer,
-				options.data.originalname as string,
-			);
-		} else {
-			throw new Error(
-				'Either a data spreadsheet file or an invoice PDF' +
-					' are mandatory for Factur-X invoices!',
-			);
-		}
+		const pdf = await this.getInvoicePdf(options);
 
 		const pdfDoc = await PDFDocument.load(pdf, { updateMetadata: false });
 
@@ -576,70 +555,6 @@ export class FormatFacturXService
 		} catch (error) {
 			console.error('Error attaching string to PDF:', error);
 			throw new Error('Error modifying PDF');
-		}
-	}
-
-	private async getPdfFromSpreadsheet(
-		spreadsheet: Buffer,
-		filename: string,
-	): Promise<Buffer> {
-		const libreoffice = this.appConfigService.get('programs').libreOffice;
-		const userDir = await tmp.tmpName();
-		const userDirUrl = url.pathToFileURL(path.resolve(userDir));
-		const parsedOriginalFilename = path.parse(filename);
-		const inputFilename = (
-			await tmp.file({ postfix: parsedOriginalFilename.ext })
-		).path;
-		await fs.writeFile(inputFilename, spreadsheet);
-		const outputDir = (await tmp.dir()).path;
-
-		const args = [
-			'--headless',
-			`-env:UserInstallation=${userDirUrl.href}`,
-			'--convert-to',
-			'pdf',
-			'--outdir',
-			outputDir,
-			inputFilename,
-		];
-		this.logger.log(
-			`Executing command '${libreoffice}' with args '${args.join(' ')}'`,
-		);
-
-		await new Promise<void>((resolve, reject) => {
-			const child = spawn(libreoffice, args, { stdio: 'inherit' });
-
-			child.on('error', err => {
-				this.logger.error(`Error spawning process: ${err.message}`);
-				reject(err);
-			});
-
-			child.on('close', code => {
-				if (code === 0) {
-					this.logger.log(`LibreOffice command executed successfully`);
-					resolve();
-				} else {
-					const errorMsg = `LibreOffice command failed with exit code ${code}`;
-					this.logger.error(errorMsg);
-					reject(new Error(errorMsg));
-				}
-			});
-		});
-
-		const parsedInputFilename = path.parse(inputFilename);
-		const outputFilename = path.join(
-			outputDir,
-			parsedInputFilename.name + '.pdf',
-		);
-
-		this.logger.log(`Reading converted file from '${outputFilename}'`);
-
-		try {
-			const pdfBuffer = await fs.readFile(outputFilename);
-			return pdfBuffer;
-		} catch (error) {
-			this.logger.error(`Error reading output PDF: ${error.message}`);
-			throw error;
 		}
 	}
 }
