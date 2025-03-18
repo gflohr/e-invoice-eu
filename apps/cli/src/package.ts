0b66787eddb54931eeb0ec2b93541d6842b1248a
Guido Flohr <guido.flohr@cantanea.com>
2025-03-18 07:54:13 +0200

    get package information

---
+++ apps/cli/src/package.ts
@@ -0,0 +1,21 @@
+export class Package {
+	public static getName(): string {
+		return 'e-invoice-eu';
+	}
+
+	public static getBugTrackerUrl(): string {
+		return 'https://github.com/gflohr/e-invoice-eu/issues';
+	}
+
+	public static getVersion(): string {
+		return '1.4.2';
+	}
+
+	public static getAuthorName(): string {
+		return 'Guido Flohr';
+	}
+
+	public static getAuthorUrl(): string {
+		return 'https://www.guido-flohr.net/';
+	}
+}
