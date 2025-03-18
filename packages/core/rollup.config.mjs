3f07ef89cdbc9499215960caa6c8b7b9a72f67da
Guido Flohr <guido.flohr@cantanea.com>
2025-03-18 08:27:53 +0200

    replace imports

--- packages/core/rollup.config.mjs
+++ packages/core/rollup.config.mjs
@@ -1,6 +1,7 @@
 import commonjs from '@rollup/plugin-commonjs';
 import json from '@rollup/plugin-json';
 import resolve from '@rollup/plugin-node-resolve';
+import replace from '@rollup/plugin-replace';
 import terser from '@rollup/plugin-terser';
 import typescript from '@rollup/plugin-typescript';
 import * as fs from 'fs';
@@ -20,6 +21,14 @@ export default [
 			sourcemap: true,
 		},
 		plugins: [
+			// Replace the import for the PDF creation.
+			replace({
+				values: {
+					'../utils/render-spreadsheet': JSON.stringify('../utils/render-spreadsheet.browser'),
+				},
+				delimiters: ["'", "'"],
+				preventAssignment: true,
+			}),
 			json(),
 			resolve({ preferBuiltins: true }),
 			commonjs(),
@@ -37,8 +46,15 @@ export default [
 			format: 'umd',
 			sourcemap: true,
 		},
-		external: ['fs'],
 		plugins: [
+			// Replace the import for the PDF creation.
+			replace({
+				values: {
+					'../utils/render-spreadsheet': JSON.stringify('../utils/render-spreadsheet.browser'),
+				},
+				delimiters: ["'", "'"],
+				preventAssignment: true,
+			}),
 			json(),
 			resolve({ preferBuiltins: true }),
 			commonjs(),
