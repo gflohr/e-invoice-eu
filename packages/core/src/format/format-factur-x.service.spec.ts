e1086161f7b501b283a18208207b1bb1f6bea47b
Guido Flohr <guido.flohr@cantanea.com>
2025-04-03 10:52:16 +0200

    check attachment name

--- packages/core/src/format/format-factur-x.service.spec.ts
+++ packages/core/src/format/format-factur-x.service.spec.ts
@@ -2,10 +2,14 @@ import { Textdomain } from '@esgettext/runtime';
 import {
 	decodePDFRawStream,
 	PDFArray,
+	PDFDict,
 	PDFDocument,
+	PDFHexString,
 	PDFName,
 	PDFNumber,
 	PDFRawStream,
+	PDFStream,
+	PDFString,
 } from 'pdf-lib';
 
 import { Invoice, InvoiceServiceOptions } from '../invoice';
@@ -81,6 +85,46 @@ async function createTestPDF(): Promise<PDFDocument> {
 
 	return pdfDoc;
 }
+
+const extractRawAttachments = (
+	pdfDoc: PDFDocument,
+): { fileName: PDFHexString | PDFString; fileSpec: PDFDict }[] => {
+	if (!pdfDoc.catalog.has(PDFName.of('Names'))) return [];
+	const Names = pdfDoc.catalog.lookup(PDFName.of('Names'), PDFDict);
+
+	if (!Names.has(PDFName.of('EmbeddedFiles'))) return [];
+	const EmbeddedFiles = Names.lookup(PDFName.of('EmbeddedFiles'), PDFDict);
+
+	if (!EmbeddedFiles.has(PDFName.of('Names'))) return [];
+	const EFNames = EmbeddedFiles.lookup(PDFName.of('Names'), PDFArray);
+
+	const rawAttachments: {
+		fileName: PDFHexString | PDFString;
+		fileSpec: PDFDict;
+	}[] = [];
+	for (let idx = 0, len = EFNames.size(); idx < len; idx += 2) {
+		const fileName = EFNames.lookup(idx) as PDFHexString | PDFString;
+		const fileSpec = EFNames.lookup(idx + 1, PDFDict);
+		rawAttachments.push({ fileName, fileSpec });
+	}
+
+	return rawAttachments;
+};
+
+const extractAttachments = (
+	pdfDoc: PDFDocument,
+): { name: string; data: Uint8Array<ArrayBufferLike> }[] => {
+	const rawAttachments = extractRawAttachments(pdfDoc);
+	return rawAttachments.map(({ fileName, fileSpec }) => {
+		const stream = fileSpec
+			.lookup(PDFName.of('EF'), PDFDict)
+			.lookup(PDFName.of('F'), PDFStream) as PDFRawStream;
+		return {
+			name: fileName.decodeText(),
+			data: decodePDFRawStream(stream).decode(),
+		};
+	});
+};
 
 describe('FormatFacturXService', () => {
 	let service: FormatFacturXService;
@@ -200,16 +244,31 @@ describe('FormatFacturXService', () => {
 	});
 
 	describe('XML attachment to PDF', () => {
-		it('should use \'factur-x.xml\' as the filename for Factur-X-Minimum', async () => {
+		it("should use 'factur-x.xml' as the filename for Factur-X-Minimum", async () => {
+			mockOptions.format = 'Factur-X-Minimum';
 
+			const pdfBytes = await service.generate(mockInvoice, mockOptions);
+			expect(pdfBytes).toBeInstanceOf(Uint8Array);
+
+			const pdfDoc = await PDFDocument.load(pdfBytes);
+			const attachments = extractAttachments(pdfDoc);
+
+			expect(attachments.length).toBe(1);
+			expect(attachments[0].name).toBe('factur-x.xml');
 		});
 
 		it('should throw an exception if attaching the Factur-X XML fails', async () => {
-			const attachMock = jest.spyOn(PDFDocument.prototype, 'attach').mockRejectedValue('no string attached');
-			const consoleMock = jest.spyOn(globalThis.console, 'error').mockImplementation(() => {});
+			const attachMock = jest
+				.spyOn(PDFDocument.prototype, 'attach')
+				.mockRejectedValue('no string attached');
+			const consoleMock = jest
+				.spyOn(globalThis.console, 'error')
+				.mockImplementation(() => {});
 
 			try {
-				await expect(service.generate(mockInvoice, mockOptions)).rejects.toThrow(
+				await expect(
+					service.generate(mockInvoice, mockOptions),
+				).rejects.toThrow(
 					'Error attaching Factur-X XML file to PDF: no string attached',
 				);
 			} finally {
