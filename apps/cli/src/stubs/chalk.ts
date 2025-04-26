565719eb4ac8b16a97432dd870581d670a334004
Guido Flohr <guido.flohr@cantanea.com>
2025-04-26 08:15:46 +0300

    add comment

--- apps/cli/src/stubs/chalk.ts
+++ apps/cli/src/stubs/chalk.ts
@@ -1,5 +1,5 @@
-// Jest chokes on the original chalk. We therefore replace it with an
-// implementation that just echoes back its argument.
+// Jest chokes on the original chalk. For testing, we therefore replace it with
+// an implementation that just echoes back its argument.
 type ChalkFn = ((str: string) => string) & { [key: string]: ChalkFn };
 
 function createChalk(): ChalkFn {
